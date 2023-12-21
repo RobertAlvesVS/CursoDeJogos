66e5ff02bd28a25617dc0d84f48e7ec84ed11980
Robert Alves <robertalvesvs@gmail.com>
2023-12-21 10:12:20 -0300

    Finish GameMemory

--- GameMemory/src/scripts/engine.js
+++ GameMemory/src/scripts/engine.js
@@ -20,5 +20,30 @@ for (let i = 0; i < emoji.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmoji[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box)
+}
+
+function handleClick(){
+  if(openCards.length < 2){
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
+  if(openCards.length == 2){
+    setTimeout(checkMath, 500);
+  }
+}
+
+function checkMath(){
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  }else{
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  }
+  openCards = [];
+  if (document.querySelectorAll("boxMatch").length === emoji.length) {
+    alert("Você ganhou, Buxa!");
+  }
 }
