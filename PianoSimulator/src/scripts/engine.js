ef681032272c9e9f9f6bfb36c6204e7cc6d1b25c
Robert Alves <robertalvesvs@gmail.com>
2023-12-26 21:15:10 -0300

    PianoSimulator Concluido

--- PianoSimulator/src/scripts/engine.js
+++ PianoSimulator/src/scripts/engine.js
@@ -1,16 +1,41 @@
 let audio = new Audio("src/tunes/a.wav");
-const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let mapedKeys = [];
 
-const playTune = (key) =>{
-    audio.src = `src/tunes/${key}.wav`;
-    audio.play();
+const volumeSlider = document.querySelector(".volume-slider input");
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const keysCheck = document.querySelector(".keys-check input");
+
+const playTune = (key) => {
+  audio.src = `src/tunes/${key}.wav`;
+  audio.play();
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 };
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.key);
-    key.addEventListener("click", () => playTune(key.dataset.key));
+  console.log(key.dataset.key);
+  key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) {
     playTune(e.key);
-})
+  }
+});
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+};
+
+volumeSlider.addEventListener("input", handleVolume);
+
+const showHideKeys = () => {
+  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+};
+
+keysCheck.addEventListener("click", showHideKeys);
